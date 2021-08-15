e46e098053c427d47c3e2c4fd2c6175cc398d07c
2nin9 <88960648+2nin9@users.noreply.github.com>
2021-08-15 18:02:45 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git! by bra1");
+        System.out.println("hello git! by 2nin9");
     }
 }
